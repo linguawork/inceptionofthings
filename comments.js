8cca6731496d851668e3c502c12ec84c568653ef
linguawork <mixedworldcitizen@gmail.com>
2025-02-06 19:50:14 +0100

    Installed K3d, docker, kubectl, created the first cluster, need to continue

---
+++ comments.js
@@ -0,0 +1,12 @@
+/*
+    Vagrant.configure("2") do |config|
+    This line begins the configuration block for Vagrant. 
+    The "2" is the version number of the Vagrant configuration 
+    format being used. Vagrant's configuration system can 
+    have different versions, and this line indicates you're 
+    using version 2, which is the most common one. The do 
+    |config| part is a Ruby block syntax that passes the 
+    config object into the block, allowing you to set various 
+    configuration options inside it.
+*/
+Vagrant.configure("2") do |config|
