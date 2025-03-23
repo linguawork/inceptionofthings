48ee60514f936e4cd347a9a604e2d38188cbf9e1
linguawork <mixedworldcitizen@gmail.com>
2025-03-23 19:54:43 +0100

    Ingress comments

--- pD2/scripts/add_hosts.sh
+++ pD2/scripts/add_hosts.sh
@@ -1,4 +1,4 @@
-# cheking and add hosts
+# checking and add hosts
 echo "192.168.56.110 app1.com" | sudo tee -a "/etc/hosts"
 echo "192.168.56.110 app2.com" | sudo tee -a "/etc/hosts"
 echo "192.168.56.110 app3.com" | sudo tee -a "/etc/hosts"
