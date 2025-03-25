044211d9118ab67b6236dd2e5205ad27bb1e6716
linguawork <mixedworldcitizen@gmail.com>
2025-03-25 09:11:29 +0100

    Part2 tested well, works. Part3 scripts work well, continue working

--- pD2/scripts/add_hosts.sh
+++
@@ -1,6 +0,0 @@
-# checking and add hosts
-echo "192.168.56.110 app1.com" | sudo tee -a "/etc/hosts"
-echo "192.168.56.110 app2.com" | sudo tee -a "/etc/hosts"
-echo "192.168.56.110 app3.com" | sudo tee -a "/etc/hosts"
-
-cat /etc/hosts
